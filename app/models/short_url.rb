c0e24e1c07c767adbd6a75d737da0b35354cd109
keang <skeang@gmail.com>
2016-12-30 09:22:58 +0800

    Add url validation

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,7 +1,11 @@
 class ShortUrl < ApplicationRecord
-  validates :original_url, presence: true
-  validates :short_path, presence: true
   before_validation :generate_short_path, on: :create
+  validates :short_path, presence: true
+
+  validates :original_url, presence: true
+  # TODO: breakdown and validate each url components,
+  # because the regex can be slow for very long url
+  validates_format_of :original_url, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?/i
 
   def generate_short_path
     self.short_path = SecureRandom.urlsafe_base64(6)
--- spec/models/short_url_spec.rb
+++ spec/models/short_url_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ShortUrl, type: :model do
-  describe "creating new ShortUrl" do
+  describe "creating new record" do
     subject { ShortUrl.create!(original_url: "https://example.com") }
 
     it { expect(subject).to be_persisted }
@@ -19,6 +19,33 @@ RSpec.describe ShortUrl, type: :model do
         expect { subject }.to change { ShortUrl.count }.by 1
         expect(ShortUrl.last.short_path).to eq "new_string"
       end
+    end
+
+    describe "original_url validation" do
+      valid_urls = ["example.com",
+                    "https://example.com/abc?as#test",
+                    "https://goo.gl/maps/GFjztXL2",
+                    "http://example.io"]
+      invalid_urls = ["postgres://example.com",
+                      "https://.com",
+                      "http://example"]
+
+      valid_urls.each do |url|
+        it "does not raise validation error for #{url}" do
+          expect {
+            ShortUrl.create!(original_url: url)
+          }.not_to raise_error
+        end
+      end
+
+      invalid_urls.each do |url|
+        it "raises validation error for #{url}" do
+          expect {
+            ShortUrl.create!(original_url: url)
+          }.to raise_error ActiveRecord::RecordInvalid
+        end
+      end
+
     end
   end
 end
