24e840a374eb3ecbf603b939ca81b8c2b3dd6558
keang <skeang@gmail.com>
2016-12-31 17:49:39 +0800

    Fix specs

--- spec/features/shorten_url_spec.rb
+++ spec/features/shorten_url_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe 'shorten url' do
   subject do
     visit root_path
-    fill_in "URL", with: "https://www.google.com"
+    fill_in "URL", with: "https://example.com"
     click_on "Shorten"
   end
 
   before do
+    stub_request(:head, "https://example.com")
     subject
   end
 
