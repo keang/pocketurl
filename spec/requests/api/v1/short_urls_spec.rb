4aed660fc96ad64e90ab8eb139216eb107cc5cc6
keang <skeang@gmail.com>
2017-01-01 11:25:35 +0800

    Add spec for api query by original_url

--- spec/requests/api/v1/short_urls_spec.rb
+++ spec/requests/api/v1/short_urls_spec.rb
@@ -5,23 +5,32 @@ RSpec.describe 'api/v1/short_urls requests' do
 
   let(:short_url) { ShortUrl.create(original_url: "https://original.com") }
   let!(:visits) { create_list(:visit, rand(100), short_url: short_url) }
+  let(:expected_hash) { {
+    "short_path" => short_url.short_path,
+    "original_url" => short_url.original_url,
+    "visits" => {
+      "count" => visits.count,
+      "unique_devices_count" => visits.pluck(:uid).uniq.count
+    },
+    "devices" => be_an(Array)
+  } }
 
-  it "returns the stats about the queried short_url" do
+  it "returns the stats when queried by short_path" do
     get api_v1_short_url_path, params: { short_path: short_url.short_path }
     expect(response).to be_ok
 
     response_hash = JSON.parse(response.body)
-    expected_hash = {
-      "short_path" => short_url.short_path,
-      "original_url" => short_url.original_url,
-      "visits" => {
-        "count" => visits.count,
-        "unique_devices_count" => visits.pluck(:uid).uniq.count
-      },
-      "devices" => be_an(Array)
-    }
     expect(response_hash).to match(expected_hash)
   end
+
+  it "returns the stats when queried by original_url" do
+    get api_v1_short_url_path, params: { original_url: short_url.original_url }
+    expect(response).to be_ok
+
+    response_hash = JSON.parse(response.body)
+    expect(response_hash).to match(expected_hash)
+  end
+
 
   it "returns 404 error if short_url is not found"
 end
